d9f2d77c1c04c320de6d130f00402e5bfdd1ae55
themuuln <zerone.offical@gmail.com>
2023-05-26 01:11:26 +0800

    Data added

---
+++ src/components/header/data/ButtonData.js
@@ -0,0 +1,7 @@
+export const buttonData = [
+  { title: "ABOUT", href: "#about" },
+  { title: "EXPERTISE", href: "#expertise" },
+  { title: "WORK", href: "#work" },
+  { title: "EXPERIENCE", href: "#experience" },
+  { title: "CONTACT", href: "#contact" },
+];
