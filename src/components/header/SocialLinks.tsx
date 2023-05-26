c7df755b7902f117d931fbd89f16f91879838e2d
themuuln <zerone.offical@gmail.com>
2023-05-26 09:12:08 +0800

    Icon bug fixed

--- src/components/header/SocialLinks.tsx
+++ src/components/header/SocialLinks.tsx
@@ -1,5 +1,4 @@
 import "./Social.css";
-import { FiArrowUpRight } from "react-icons/fi";
 
 const SocialLinks = ({ children, href }: any) => {
   return (
@@ -7,7 +6,21 @@ const SocialLinks = ({ children, href }: any) => {
       <li className="flex transition-all items-center gap-1 hover:text-gray-200">
         {children}
         <div className=" transition-all">
-          <FiArrowUpRight />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="feather feather-arrow-up-right"
+          >
+            <line x1="7" y1="17" x2="17" y2="7"></line>
+            <polyline points="7 7 17 7 17 17"></polyline>
+          </svg>
         </div>
       </li>
     </a>
